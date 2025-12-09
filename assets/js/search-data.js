12cab46ad37ea6c72978bc651b72969e2ab9ffc4
xun04 <162805063+xun04@users.noreply.github.com>
2025-12-09 10:56:29 +0000

    Deploying to gh-pages from @ xun04/xun04.github.io@380caa49c36c0413c19ac670b553b493c72ef642 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,17 +38,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/photo-gallery/";
           
         },
-      },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
       },{id: "post-a-post-with-tabs",
         
           title: "a post with tabs",
@@ -269,17 +258,6 @@ ninja.data = [{
             window.location.href = "/blog/2022/giscus-comments/";
           
         },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
       },{id: "post-a-post-with-redirect",
         
           title: "a post with redirect",
@@ -393,8 +371,8 @@ ninja.data = [{
           section: "News",},{id: "news-i-received-the-2024-university-level-third-class-scholarship",
           title: 'I received the 2024 University-Level Third-Class Scholarship! 🥳',
           description: "",
-          section: "News",},{id: "news-my-overall-assessment-score-ranked-4th-out-of-95-students-in-the-2024-2025-academic-year-️",
-          title: 'My overall assessment score ranked 4th out of 95 students in the 2024-2025...',
+          section: "News",},{id: "news-my-overall-assessment-score-ranked-4th-out-of-99-students-in-the-2024-2025-academic-year-️",
+          title: 'My overall assessment score ranked 4th out of 99 students in the 2024-2025...',
           description: "",
           section: "News",},{id: "news-our-work-robuq-is-released-paper-github-arxiv",
           title: 'Our work RobuQ is released: Paper | GitHub | Arxiv. 🌱',
